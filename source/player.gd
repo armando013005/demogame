d2c1103b92f209c128921d3c5072477ad7f2c82f
Armando Orozco <ao92436@student.sac.edu>
2024-09-01 14:16:01 -0700

    added rocks and trees

--- source/player.gd
+++ source/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
-const vel = 100;
+const vel = 250;
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	velocity = direction * vel
 	move_and_slide()
 	
-	get_node("OneCharacter").play_walk_animation()
+	if velocity.length() > 0.0:
+		get_node("OneCharacter").play_walk_animation()
